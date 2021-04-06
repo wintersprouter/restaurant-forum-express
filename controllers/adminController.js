c785dd16a0d4a5c6ea2b67c40927530fd93af3d4
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-06 16:16:38 +0800

    Add admin restaurant delete feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -65,6 +65,15 @@ const adminController = {
       })
     })
   },
+  deleteRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id)
+    .then((restaurant) => {
+      restaurant.destroy()
+      .then((restaurant) => {
+        res.redirect('/admin/restaurants')
+      })
+    })
+  }
 }
 
 module.exports = adminController

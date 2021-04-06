547197d5ab1c3793b0281e6fb95ccd2ee1596823
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-06 15:06:44 +0800

    Install passport and passport-local

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models')
 const bodyParser = require('body-parser') 
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
@@ -13,8 +14,11 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')

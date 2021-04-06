59ddccd8fe601ffe164d9b4f9db0012e0bcbf070
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-06 15:53:55 +0800

    Add admin restaurant create form

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,10 @@ const adminController = {
     return Restaurant.findAll({raw: true}).then(restaurants => {
       return res.render('admin/restaurants', {restaurants: restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ module.exports = (app, passport) => {
   
   app.get('/admin',authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants',authenticatedAdmin,adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)

7e109f3107092ec0d52fc085c8f6da66e60df8b9
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-06 16:09:53 +0800

    Update admin create page for editing usage

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -36,7 +36,13 @@ const adminController = {
         restaurant: restaurant
       })
     })
-  }
+  },
+
+  editRestaurant: (req, res) => {
+  return Restaurant.findByPk(req.params.id, {raw:true}).then(restaurant => {
+    return res.render('admin/create', { restaurant: restaurant } )
+  })
+}
 }
 
 module.exports = adminController

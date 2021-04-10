adfa7847b628ef27b27a5542629e2a49a4970d8c
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-10 16:15:29 +0800

    Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars') 
 
 app.use(bodyParser.urlencoded({extended: true}))

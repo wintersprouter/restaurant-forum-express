1da68aac5ed7994984d6469e27e7dded20774935
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-18 23:23:25 +0800

    Add addFollowing and removeFollowing to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 const Favorite = db.Favorite
+const Followship = db.Followship
 
 const userController = {
   signUpPage: (req, res) => {
@@ -86,7 +87,31 @@ const userController = {
       users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
       return res.render('topUser', { users: users })
     })
-  }
+  },
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId
+    })
+  .then((followship) => {
+    return res.redirect('back')
+  })
+},
+
+removeFollowing: (req, res) => {
+  return Followship.findOne({where: {
+    followerId: req.user.id,
+    followingId: req.params.userId
+  }})
+    .then((followship) => {
+      followship.destroy()
+      .then((followship) => {
+        return res.redirect('back')
+      })
+    })
+}
+
+  
 }
 
 module.exports = userController

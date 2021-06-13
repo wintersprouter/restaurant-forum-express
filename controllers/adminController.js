c10dd5f006da4738b5d2dd27c9b85e1a36d658a5
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-14 00:36:25 +0800

    refactor: refactor adminController postRestaurant feature by async/await.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -29,42 +29,49 @@ const adminController = {
     }
   },
 
-  postRestaurant: (req, res) => {
-    if (!req.body.name) {
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
-    }
+  postRestaurant: async (req, res) => {
+    try {
+      const { name, tel, address, opening_hours, description, categoryId } = req.body
+      if (!name) {
+        req.flash('error_messages', "name didn't exist")
+        return res.redirect('back')
+      }
 
-    const { file } = req
-    if (file) {
-      imgur.setClientID(IMGUR_CLIENT_ID)
-      imgur.upload(file.path, (err, img) => {
-        return Restaurant.create({
-          name: req.body.name,
-          tel: req.body.tel,
-          address: req.body.address,
-          opening_hours: req.body.opening_hours,
-          description: req.body.description,
-          image: file ? img.data.link : null,
-          CategoryId: req.body.categoryId
-        }).then((restaurant) => {
-          req.flash('success_messages', 'restaurant was successfully created')
-          return res.redirect('/admin/restaurants')
+      const { file } = req
+      if (file) {
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        imgur.upload(file.path, async (err, img) => {
+          try {
+            await Restaurant.create({
+              name,
+              tel,
+              address,
+              opening_hours,
+              description,
+              image: file ? img.data.link : null,
+              CategoryId: categoryId
+            })
+            req.flash('success_messages', 'restaurant was successfully created')
+            return res.redirect('/admin/restaurants')
+          } catch (err) {
+            console.log(err)
+          }
         })
-      })
-    } else {
-      return Restaurant.create({
-        name: req.body.name,
-        tel: req.body.tel,
-        address: req.body.address,
-        opening_hours: req.body.opening_hours,
-        description: req.body.description,
-        image: null,
-        CategoryId: req.body.categoryId
-      }).then((restaurant) => {
+      } else {
+        await Restaurant.create({
+          name,
+          tel,
+          address,
+          opening_hours,
+          description,
+          image: null,
+          CategoryId: categoryId
+        })
         req.flash('success_messages', 'restaurant was successfully created')
         return res.redirect('/admin/restaurants')
-      })
+      }
+    } catch (err) {
+      console.log(err)
     }
   },
 

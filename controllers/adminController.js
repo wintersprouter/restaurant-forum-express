56591417779bb6e9bac243b69f24292f279849bd
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-14 00:15:47 +0800

    refactor: refactor adminController getRestaurants feature by async/await.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,21 +1,24 @@
 const db = require('../models')
-const Restaurant = db.Restaurant
-const User = db.User
-const Category = db.Category
+const {Restaurant,User,Category} = db
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
+  getRestaurants: async (req, res) => {
+    try{
+    const restaurants = await Restaurant.findAll({
       raw: true,
       nest: true,
       include: [Category],
       order: [['updatedAt', 'DESC']]
-    }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
     })
+    
+    return res.render('admin/restaurants', { restaurants: restaurants })
+    
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   createRestaurant: (req, res) => {

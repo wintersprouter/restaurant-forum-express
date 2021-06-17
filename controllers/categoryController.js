b60bf2a6bacbaa8ff9fdd39809c372cca502ab03
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-06-17 23:54:45 +0800

    refactor: refactor categoryController by async/await

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,59 +1,65 @@
 const db = require('../models')
 const Category = db.Category
 const categoryController = {
-  getCategories: (req, res) => {
-    return Category.findAll({
-      raw: true,
-      nest: true
-    }).then(categories => {
-      if (req.params.id) {
-        Category.findByPk(req.params.id)
-          .then((category) => {
-            return res.render('admin/categories', {
-              categories: categories,
-              category: category.toJSON()
-            })
-          })
+  getCategories: async (req, res) => {
+    try {
+      const id = req.params.id
+      const categories = await Category.findAll({ raw: true, nest: true })
+      if (id) {
+        const category = await Category.findByPk(id)
+        return res.render('admin/categories', {
+          categories,
+          category: category.toJSON()
+        })
       } else {
-        return res.render('admin/categories', { categories: categories })
+        return res.render('admin/categories', { categories })
       }
-    })
-  },
-  postCategory: (req, res) => {
-    if (!req.body.name) {
-      req.flash('error_messages', 'name didn\'t exist')
-      return res.redirect('back')
-    } else {
-      return Category.create({
-        name: req.body.name
-      })
-        .then((category) => {
-          res.redirect('/admin/categories')
-        })
+    } catch (err) {
+      console.log(err)
     }
   },
-  putCategory: (req, res) => {
-    if (!req.body.name) {
-      req.flash('error_messages', 'name didn\'t exist')
+  postCategory: async (req, res) => {
+    const { name } = req.body
+    if (!name) {
+      req.flash('error_messages', '請填寫名稱')
       return res.redirect('back')
     } else {
-      return Category.findByPk(req.params.id)
-        .then((category) => {
-          category.update(req.body)
-            .then((category) => {
-              res.redirect('/admin/categories')
-            })
-        })
+      try {
+        await Category.create({ name })
+        req.flash('success_messages', '新增類別成功！')
+        res.redirect('/admin/categories')
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
-  deleteCategory: (req, res) => {
-    return Category.findByPk(req.params.id)
-      .then((category) => {
-        category.destroy()
-          .then((category) => {
-            res.redirect('/admin/categories')
-          })
-      })
+  putCategory: async (req, res) => {
+    const { name } = req.body
+    const id = req.params.id
+    if (!name) {
+      req.flash('error_messages', '請填寫名稱')
+      return res.redirect('back')
+    } else {
+      try {
+        const category = await Category.findByPk(id)
+        category.update(name)
+        req.flash('success_messages', '修改類別成功！')
+        res.redirect('/admin/categories')
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  deleteCategory: async (req, res) => {
+    const id = req.params.id
+    try {
+      const category = await Category.findByPk(id)
+      category.destroy()
+      req.flash('success_messages', '刪除類別成功！')
+      res.redirect('/admin/categories')
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 module.exports = categoryController
